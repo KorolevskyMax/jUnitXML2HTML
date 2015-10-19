a036b03308863e21d26bb4ed253a739ddf93077e
KorolevskyMax <korolevsky.max@gmail.com>
2015-10-19 16:01:29 +0300

    fixed installation problem

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-from codecs import open
+from io import open
 from os import path
 
 with open(path.join(path.dirname(__file__), "README.md")) as readme:
